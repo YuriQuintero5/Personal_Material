41af7679624b2484590f08e5fdebdb7b3152d478
Yuri Quintero <ingenieraquintero@gmail.com>
2020-05-30 08:33:23 -0500

    adaptador funcional

--- app/src/main/java/com/example/persona_material/AdaptadorPersona.java
+++ app/src/main/java/com/example/persona_material/AdaptadorPersona.java
@@ -1,5 +1,6 @@
 package com.example.persona_material;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,26 +12,32 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class AdaptadorPersona extends RecyclerView.Adapter<AdaptadorPersona.PersonaViewHolder>{
-    private ArrayList<Persona> persona;
+    private ArrayList<Persona> personas;
 
     public AdaptadorPersona(ArrayList<Persona> persona){
-        this.persona = persona;
+        this.personas = persona;
     }
 
     @NonNull
     @Override
     public PersonaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+       View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pesona, parent, false);
+       return new PersonaViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PersonaViewHolder holder, int position) {
+        Persona p = personas.get(position);
+        holder.foto.setImageResource(p.getFoto());
+        holder.cedula.setText(p.getCedula());
+        holder.nombre.setText(p.getNombre());
+        holder.apellido.setText(p.getApellido());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return personas.size();
     }
 
 
