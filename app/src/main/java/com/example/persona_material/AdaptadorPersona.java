08b98f50f9a78639c54de0fe5a45cec34012ed53
Yuri Quintero <ingenieraquintero@gmail.com>
2020-05-30 12:05:33 -0500

    Evento click de listado funcionando correctamente

--- app/src/main/java/com/example/persona_material/AdaptadorPersona.java
+++ app/src/main/java/com/example/persona_material/AdaptadorPersona.java
@@ -13,9 +13,12 @@ import java.util.ArrayList;
 
 public class AdaptadorPersona extends RecyclerView.Adapter<AdaptadorPersona.PersonaViewHolder>{
     private ArrayList<Persona> personas;
+    private OnPersonaClickListener clickListener;
 
-    public AdaptadorPersona(ArrayList<Persona> persona){
-        this.personas = persona;
+
+    public AdaptadorPersona(ArrayList<Persona> personas, OnPersonaClickListener clickListener){
+        this.personas = personas;
+        this.clickListener = clickListener;
     }
 
     @NonNull
@@ -27,12 +30,18 @@ public class AdaptadorPersona extends RecyclerView.Adapter<AdaptadorPersona.Pers
 
     @Override
     public void onBindViewHolder(@NonNull PersonaViewHolder holder, int position) {
-        Persona p = personas.get(position);
+        final Persona p = personas.get(position);
         holder.foto.setImageResource(p.getFoto());
         holder.cedula.setText(p.getCedula());
         holder.nombre.setText(p.getNombre());
         holder.apellido.setText(p.getApellido());
 
+        holder.v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickListener.onPersonaClick(p);
+            }
+        });
     }
 
     @Override
@@ -56,5 +65,9 @@ public class AdaptadorPersona extends RecyclerView.Adapter<AdaptadorPersona.Pers
             nombre = v.findViewById(R.id.lblNombre);
             apellido = v.findViewById(R.id.lblApellido);
         }
+
+      }
+    public interface OnPersonaClickListener{
+        void onPersonaClick(Persona p);
     }
 }
